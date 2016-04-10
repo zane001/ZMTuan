29e28df2bdefadb2b812261b13d2cc8b8eddd739
zane001 <90374@qq.com>
2016-04-11 00:14:11 +0800

    文件夹变更

--- ZMTuan/Controller/ViewController.swift
+++ ZMTuan/Controller/ViewController.swift
@@ -12,7 +12,10 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        let vc = UIAlertController(title: "fuck", message: "you", preferredStyle: .Alert)
+        vc.addAction(UIAlertAction(title: "lol", style: .Default, handler: nil))
+        self.presentViewController(vc, animated: true, completion: nil)
     }
 
     override func didReceiveMemoryWarning() {
